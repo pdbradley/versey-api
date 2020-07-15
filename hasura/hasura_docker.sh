cf66f1e778c18781e5719fa2b407c7b1bf689bed
Philip Bradley <pdbradley@gmail.com>
2020-07-15 10:12:46 -0400

    more hasura setup

---
+++ hasura/hasura_docker.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# see .env.example; the hasura env vars should be set there
+
+docker run -d -p 8080:8080 --env-file ./.env \
+       -e HASURA_GRAPHQL_DATABASE_URL \
+       -e HASURA_GRAPHQL_ADMIN_SECRET \
+       -e HASURA_GRAPHQL_JWT_SECRET \
+       -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
+       hasura/graphql-engine:v1.2.1
+
--- hasura_docker.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-docker run -d -p 8080:8080 \
-       -e HASURA_GRAPHQL_DATABASE_URL=postgres://philip:@host.docker.internal:5432/jackdawg_api_development \
-       -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
-       hasura/graphql-engine:v1.2.2
